ec0569279e4634677fafe3d1d7e2f4dd2269dd90
Omkar-Chaskar <Omkarchaskar1402@gmail.com>
2023-02-07 18:13:39 +0530

    feat : added navbar
    added navbar with button

---
+++ src/component/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <header className='navbar'>
+        <button className='nav-button'>Get Users</button>
+    </header>
+  )
+}
+
+export default Navbar
